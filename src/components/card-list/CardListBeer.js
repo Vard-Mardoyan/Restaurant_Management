8b275032096554e351c8e8f6b087cc5f1eb0c5bc
Lusine Sargsyan <sargsyanlq@gmail.com>
2021-06-05 12:02:12 +0400

    render price with currency

--- src/components/card-list/CardListBeer.js
+++ src/components/card-list/CardListBeer.js
@@ -7,10 +7,10 @@ export default function CardListBeer({onItemClick, items}) {
   return (
     <div>
       <ul className="list">
-      {items.map(({ id, name, description, image_url, volume, price}) => {
+      {items.map(({ id, name, description, image_url, volume}) => {
         return (
           <div key={id}>
-            <CardBeerComponent {...{id, name, description, image_url, volume, price}} onItemClick={onItemClick}/>
+            <CardBeerComponent {...{id, name, description, image_url, volume}} onItemClick={onItemClick}/>
           </div>
         );
       })}
