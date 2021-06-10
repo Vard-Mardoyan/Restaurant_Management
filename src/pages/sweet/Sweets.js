101dfb2131fa4f2dce5a958b704e06d3cf365357
Lusine Sargsyan <sargsyanlq@gmail.com>
2021-06-10 09:48:50 +0400

    recover pagination

--- src/pages/sweet/Sweets.js
+++ src/pages/sweet/Sweets.js
@@ -6,13 +6,31 @@ import { SWEETS_DATA_FROM_LOCAL_STORAGE } from "../../helper/local-storage";
 import "./SweetsStyle.css";
 
 export default function Sweets() {
-	const [data, setData] = useState(SWEETS_DATA_FROM_LOCAL_STORAGE);
-	console.log(SWEETS_DATA_FROM_LOCAL_STORAGE, "SWEETS_DATA_FROM_LOCAL_STORAGE");
+	const [data, setData] = useState(DATA["SWEETS_DATA_FROM_LOCAL_STORAGE"]);
+	
+	//console.log(SWEETS_DATA_FROM_LOCAL_STORAGE, "SWEETS_DATA_FROM_LOCAL_STORAGE");
+	//console.log(DATA['SWEETS_DATA_FROM_LOCAL_STORAGE'], 'typeData-----');
+  
+	const [currentPage, setCurrentPage] = useState(1);
+  const [perPageData] = useState(8);
+  const [isLoading, setIsLoading] = useState(false); 
+
+	// useEffect(() => {
+	// 	setData("SWEETS_DATA_FROM_LOCAL_STORAGE");
+	// }, []);
+
+	const indexOfLastData = currentPage * perPageData;
+  const indexOfFirstData = indexOfLastData  - perPageData;
+  const currentData = data.slice(indexOfFirstData, indexOfLastData);
+
+
+	const paginate = (number) => setCurrentPage(number);
 
 	return (
 		<div className="sweet-container">
 			<h1 className="title">Sweets </h1>
-			<CardList dataType="SWEETS_DATA_FROM_LOCAL_STORAGE" />
+			<CardList dataType="SWEETS_DATA_FROM_LOCAL_STORAGE" paginatedData={currentData}/>
+			<Pagination perPageData={perPageData} totalData={data} paginate={paginate} />
 		</div>
 	);
 }
