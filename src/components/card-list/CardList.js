75cf90dfb4970d19402c8f3567ad95aa4453bcca
Lusine Sargsyan <sargsyanlq@gmail.com>
2021-06-15 20:56:00 +0400

    remove fragment from CardList

--- src/components/card-list/CardList.js
+++ src/components/card-list/CardList.js
@@ -1,53 +1,52 @@
 import { useCartContext } from "../../context/cart/CartState";
-import {SWEETS_DATA_FROM_LOCAL_STORAGE, FOODS_DATA_FROM_LOCAL_STORAGE} from "../../helpers/local-storage";
+import {
+  SWEETS_DATA_FROM_LOCAL_STORAGE,
+  FOODS_DATA_FROM_LOCAL_STORAGE,
+} from "../../helpers/local-storage";
 import CardComponent from "../card/Card";
-
 
 import "./CardListStyle.css";
 
-export default function CardList({ data}) {
-	const { addToCart } = useCartContext();
-	//console.log(data, "data:::::");
+export default function CardList({ data }) {
+  const { addToCart } = useCartContext();
+  //console.log(data, "data:::::");
 
-	
-	if (data === SWEETS_DATA_FROM_LOCAL_STORAGE || data === FOODS_DATA_FROM_LOCAL_STORAGE) {
-		return (
-			<>
-				<ul className="list">
-					{data?.map(({ id, name, image, kitchen, price, quantity = 0 }) => {
-						return (
-							<div key={id}>
-								<CardComponent
-									{...{ id, name, image, kitchen, price, quantity }}
-									addToCart={addToCart}
-								/>
-							</div>
-						);
-					})}
-				</ul>
-			</>
-		);
-	}
+  if (
+    data === SWEETS_DATA_FROM_LOCAL_STORAGE ||
+    data === FOODS_DATA_FROM_LOCAL_STORAGE
+  ) {
+    return (
+      <ul className="list">
+        {data?.map(({ id, name, image, kitchen, price, quantity = 0 }) => {
+          return (
+            <div key={id}>
+              <CardComponent
+                {...{ id, name, image, kitchen, price, quantity }}
+                addToCart={addToCart}
+              />
+            </div>
+          );
+        })}
+      </ul>
+    );
+  }
 
-	return (
-		<>
-			<ul className="list">
-				{data?.map(
-					({ id, name, image, kitchen, description, price, quantity = 0 }) => {
-						return (
-							<div key={id}>
-								<CardComponent
-									{...{ id, name, image, kitchen, description, price, quantity }}
-									addToCart={addToCart}
-								/>
-							</div>
-						);
-					}
-				)}
-			</ul>
-		</>
-	);
-
+  return (
+    <ul className="list">
+      {data?.map(
+        ({ id, name, image, kitchen, description, price, quantity = 0 }) => {
+          return (
+            <div key={id}>
+              <CardComponent
+                {...{ id, name, image, kitchen, description, price, quantity }}
+                addToCart={addToCart}
+              />
+            </div>
+          );
+        }
+      )}
+    </ul>
+  );
 }
 
 // export default function CardList({ dataType, paginatedData }) {
@@ -56,7 +55,6 @@ export default function CardList({ data}) {
 // 	console.log(dataType, "dataType");
 // 	console.log(paginatedData, "data");
 
-	
 // 	if (DATA[dataType] === "SWEETS_DATA_FROM_LOCAL_STORAGE" || DATA[dataType] === "FOODS_DATA_FROM_LOCAL_STORAGE") {
 // 		return (
 // 			<div>
@@ -96,6 +94,3 @@ export default function CardList({ data}) {
 // 	);
 
 // }
-
-
-
--- src/components/card-list/CardListBeer.js
+++ src/components/card-list/CardListBeer.js
@@ -2,21 +2,20 @@ import CardBeerComponent from "../card/CardBeer";
 import "./CardListStyle.css";
 
 export default function CardListBeer({ addToCart, items }) {
-	return (
-		    <div>
-		      <ul className="list">
-		      {items?.map(({ id, name, description, image_url, volume}) => {
-		        return (
-		
-		          <div key={id}>
-		            <CardBeerComponent {...{id, name, description, image_url, volume}} addToCart={addToCart}/>
-		          </div>
-		        );
-		      })}
-		      </ul>
-		    </div>
-		  );
-		
-	
+  return (
+    <div>
+      <ul className="list">
+        {items?.map(({ id, name, description, image_url, volume }) => {
+          return (
+            <div key={id}>
+              <CardBeerComponent
+                {...{ id, name, description, image_url, volume }}
+                addToCart={addToCart}
+              />
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
-
